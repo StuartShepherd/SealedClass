bf73fe727b00d7cc1611924ba65ac7b77c06ba61
stuartjshepherd <stuart@createstudios.com.au>
2020-03-06 08:45:16 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SealedClass
+{
+    class Program
+    {
+        public sealed class Name
+        {
+            public string FirstName;
+            public string LastName;
+        }
+
+        // Program.Employee: cannot derive from sealed type Program.Name
+        //public class Employee : Name
+        //{
+
+        //}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sealed Class");
+            Console.WriteLine("A class with sealed keyword indicates that class is sealed to prevent inheritance. Sealed class cannot inheritance.");
+            Console.WriteLine("");
+
+            // You are unable to inherit the Name class.
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Sealed Class Example
+A class with sealed keyword indicates that class is sealed to prevent inheritance. Sealed class cannot inheritance.
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
